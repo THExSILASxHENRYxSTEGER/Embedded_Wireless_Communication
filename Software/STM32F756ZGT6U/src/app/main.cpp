de31c0f13af4d41d4e7d0cc80ecea647abd5de8c
xsilasx <steger@campus.tu-berlin.de>
2026-08-22 22:51:32 +0200

    Prompt the user to give input which is then saved and printed back to the console.

--- Software/STM32F756ZGT6U/src/app/main.cpp
+++ Software/STM32F756ZGT6U/src/app/main.cpp
@@ -1,20 +1,48 @@
-#include "rcc/rcc.hpp"
-#include "rcc/rcc_bits.hpp"
+#include "global/clock_vars.hpp"
 #include "gpio/gpio.hpp"
 #include "gpio/gpio_bits.hpp"
+#include "nvic/nvic_bits.hpp"
+#include "nvic/nvic.hpp"
+#include "rcc/rcc.hpp"
+#include "rcc/rcc_bits.hpp"
+#include "input_buffer/input_buffer.hpp"
+#include "usart/usart.hpp"
 
 #include <cstdint>
 
-
 int main(){
   
-  rcc::rcc.peripheral_clk_enable(rcc::RCC_AHB1ENR::GPIOFEN);
-  
-  gpio::port_F.set_pin_pup_output(gpio::GPIO_PIN::GPIO_PIN_12);
+  // Enable clock for pins on port D for usart 2
+  rcc::rcc.peripheral_clk_enable(rcc::RCC_AHB1ENR::GPIODEN);
+  // Enable clock of usart 2
+  rcc::rcc.peripheral_clk_enable(rcc::RCC_APB1ENR::USART2EN);  
+  // Enable GPIO USART2 TX, RX, and CK
+  gpio::port_D.enable_and_set_pin_AF(gpio::GPIO_PIN::GPIO_PIN_5, gpio::GPIO_AF::AF7, gpio::GPIO_OSPEEDR::MEDIUM_SPEED); // use variadic function here if works
+  gpio::port_D.enable_and_set_pin_AF(gpio::GPIO_PIN::GPIO_PIN_6, gpio::GPIO_AF::AF7, gpio::GPIO_OSPEEDR::MEDIUM_SPEED);
+  gpio::port_D.enable_and_set_pin_AF(gpio::GPIO_PIN::GPIO_PIN_7, gpio::GPIO_AF::AF7, gpio::GPIO_OSPEEDR::MEDIUM_SPEED);
 
-  gpio::port_F.pull_pin_up(gpio::GPIO_PIN::GPIO_PIN_12);
+  // Initialize NVIC 
+  nvic::nvic.NVIC_EnableIRQ(nvic::IRQn::USART2);
 
-  for(;;);
+  // Initialize USART
+  usart::usart_2.init(CLK_FREQ::_16_MHZ, BAUD_RATE::_9_6_K);
+
+  // Send text over USART
+  usart::usart_2.send_string("Hello, what would you like to tell me?:\n");
+
+  // Receive an answer
+  while(true)
+  {
+    // Wait until usart has read all characters of the search request
+    while(!input_buffer::ib.answered());
+    // Print the answer
+    usart::usart_2.send_string("\nYou have entered:\n");
+    input_buffer::ib.print(usart::usart_2);
+    // Prompt the user for new input
+    usart::usart_2.send_string("\nYou can now tell me something else.\n");
+    // Empty the input character data buffer
+    input_buffer::ib.clear();
+  }  
   
   return 0;
 }
