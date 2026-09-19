4f0b2e29e79f1b4f27741f348feb2c13665cb6f8
xsilasx <steger@campus.tu-berlin.de>
2026-09-19 13:34:51 +0200

    Comment on the basic timer functions and registers.

--- Software/STM32F756ZGT6U/include/basic_timer/basic_timer.hpp
+++ Software/STM32F756ZGT6U/include/basic_timer/basic_timer.hpp
@@ -12,8 +12,19 @@ namespace basic_timer
   {
     public:
       
+      /**
+       * @brief Construct a new Basic Timer object
+       * 
+       * @param basic_timer_address The physical memory address of the corresponding basic timer peripheral.
+       */
       BasicTimer(const std::uint32_t basic_timer_address);
       
+      /**
+       * @brief Wait idle for the set amount of time
+       * 
+       * @param n How many time units should be waited.
+       * @param duration The time unit to be waited i.e. microseconds, milliseconds, seconds, etc.
+       */
       void wait_for(std::uint32_t n, DURATION duration);
       
       ~BasicTimer() = default;
@@ -22,12 +33,33 @@ namespace basic_timer
       
       volatile BASIC_TIMER_REGS * basic_timer_regs_;
 
+      /**
+       * @brief Set the actual timer to run for the given time parameters. 
+       * 
+       * @param psc The timer prescaler.
+       * @param arr The timer auto reload  register.
+       */
       void activate_timer(PSC psc, std::uint32_t arr);
 
+      /**
+       * @brief Query if an update event occured.
+       * 
+       * If the counter hits the auto-reload registers' stored value an update event is caused.
+       * This signals that the time period set out for has expired. 
+       * 
+       * @return true An update event occured.
+       * @return false An update event has not occured.
+       */
       bool update_event();
       
+      /**
+       * @brief Clear the fired update event flag.
+       */
       void clear_update();
 
+      /**
+       * @brief Deactivate the timer for future reinitialization.
+       */
       void deactivate_timer();
   };  
 
--- Software/STM32F756ZGT6U/include/basic_timer/basic_timer_regs.hpp
+++ Software/STM32F756ZGT6U/include/basic_timer/basic_timer_regs.hpp
@@ -4,16 +4,22 @@
 
 namespace basic_timer
 {
+
+  /**
+   * @brief Basic timer peripheral registers.
+   * 
+   * To manipulate the register addresses of any basic timer use this struct.
+   */
   struct BASIC_TIMER_REGS {
-    volatile std::uint32_t CR1;
-    volatile std::uint32_t CR2;
-    volatile std::array<std::uint32_t, 1> UNUSED_MEM_0;
-    volatile std::uint32_t DIER;
-    volatile std::uint32_t SR;
-    volatile std::uint32_t EGR;
-    volatile std::array<std::uint32_t, 3> UNUSED_MEM_1;
-    volatile std::uint32_t CNT;
-    volatile std::uint32_t PSC;
-    volatile std::uint32_t ARR;
+    volatile std::uint32_t CR1;                         // Control Register 1
+    volatile std::uint32_t CR2;                         // Control Register 2
+    volatile std::array<std::uint32_t, 1> UNUSED_MEM_0; // Unused memory
+    volatile std::uint32_t DIER;                        // DMA/Interrupt enable register
+    volatile std::uint32_t SR;                          // Status Register
+    volatile std::uint32_t EGR;                         // Event generation register
+    volatile std::array<std::uint32_t, 3> UNUSED_MEM_1; // Unused memory
+    volatile std::uint32_t CNT;                         // Counter 
+    volatile std::uint32_t PSC;                         // Prescaler 
+    volatile std::uint32_t ARR;                         // Aut-reload register
   };
 }
